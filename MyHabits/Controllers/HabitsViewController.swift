42c9e8692767705c9f224a99ef5fd7f2d894f3ac
apbrandom <apbrandom@gmail.com>
2023-03-14 16:49:49 +0100

    Design for HabitsViewController and infoViewController

--- MyHabits/Controllers/HabitsViewController.swift
+++ MyHabits/Controllers/HabitsViewController.swift
@@ -9,26 +9,45 @@ import UIKit
 
 class HabitsViewController: UIViewController {
     
+    //MARK: - Subviews
+    
+    private lazy var addButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        button.tintColor = .systemPurple
+        return button
+    }()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+
+    }
+    
+    //MARK: - Action
+    
+    @objc func addButtonTapped() {
+        print("Hello")
     }
     
     //MARK: - Private
     
     private func setupView() {
         title = "Привычки"
-        view.backgroundColor = .systemGray5
-        
+        navigationItem.rightBarButtonItem = addButton
+        view.backgroundColor = .systemBackground
+
         tabBarItem = UITabBarItem(
             title: "Привычки",
             image: UIImage(systemName: "rectangle.grid.1x2.fill"),
             tag: 0)
-        
+        tabBarItem.selectedImage = UIImage(systemName: "rectangle.grid.1x2.fill")?.withTintColor(UIColor.systemPurple, renderingMode: .alwaysOriginal)
+        tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemPurple], for: .selected)
+
+
     }
     
-    
 }
+
--- MyHabits/Controllers/InfoViewController.swift
+++ MyHabits/Controllers/InfoViewController.swift
@@ -32,7 +32,7 @@ class InfoViewController: UIViewController {
     private lazy var headerLabel: UILabel = {
         let textView = UILabel()
         textView.text = "Привычка за 21 день"
-        textView.font = .systemFont(ofSize: 20, weight: .medium)
+        textView.font = .systemFont(ofSize: 20, weight: .semibold)
         textView.backgroundColor = .systemBackground
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
@@ -44,9 +44,6 @@ class InfoViewController: UIViewController {
         textView.font = .systemFont(ofSize: 17, weight: .regular)
         textView.lineBreakMode = .byWordWrapping
         textView.numberOfLines = 100
-//        textView.isEditable = false
-//        textView.isSelectable = false
-//        textView.isScrollEnabled = false
         textView.backgroundColor = .systemBackground
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
@@ -64,13 +61,15 @@ class InfoViewController: UIViewController {
     
     private func setupView() {
         title = "Информация"
+        navigationItem.largeTitleDisplayMode = .never
         view.backgroundColor = .systemBackground
-        
         
         tabBarItem = UITabBarItem(
             title: "Информация",
             image: UIImage(systemName: "info.circle.fill"),
             tag: 0)
+        tabBarItem.selectedImage = UIImage(systemName: "info.circle.fill")?.withTintColor(UIColor.systemPurple, renderingMode: .alwaysOriginal)
+        tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.systemPurple], for: .selected)
     }
     
     private func setupSubviews() {
@@ -84,22 +83,17 @@ class InfoViewController: UIViewController {
     private func setupConstrains() {
         let safeArea = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
-            
             scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-//
+
             contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-
-            
-//            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-//            contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
             
             headerLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 22),
             headerLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
@@ -109,7 +103,6 @@ class InfoViewController: UIViewController {
             infoTextView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             infoTextView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             infoTextView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-            
         ])
     }
     
